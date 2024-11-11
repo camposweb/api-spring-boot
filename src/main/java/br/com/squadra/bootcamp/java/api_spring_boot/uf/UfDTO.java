d7b7f7996e7a4bcde700b71f3444a3d0034017e8
Leandro Campos <camposweb@gmail.com>
2024-11-11 16:34:17 -0300

    fix: Corrigindo os tipos de validações do UfDTO

--- src/main/java/br/com/squadra/bootcamp/java/api_spring_boot/uf/UfDTO.java
+++ src/main/java/br/com/squadra/bootcamp/java/api_spring_boot/uf/UfDTO.java
@@ -1,24 +1,23 @@
 package br.com.squadra.bootcamp.java.api_spring_boot.uf;
 
-import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record UfDTO(
 
         @NotBlank
-        @Length(max = 9)
-        Long codigoUf,
-
-        @NotBlank
-        @Length(max = 3)
+        @Length(max = 3, message = "Deve conter no máximo 3 caracteres")
         String sigla,
 
         @NotBlank
-        @Length(max = 60)
+        @Length(max = 60, message = "Deve conter no máximo 60 caracteres")
         String nome,
 
-        @NotBlank
-        @Length(max = 3)
+        @NotNull
+        @Range(min = 1, max = 2, message = "Deve conter 1 para ativo ou 2 para desativado")
         int status
 ) {
 }

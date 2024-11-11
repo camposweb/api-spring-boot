6cf9f8f1cd33f7780a9e01a1bfd0d3ee627466ed
Leandro Campos <camposweb@gmail.com>
2024-11-11 16:49:19 -0300

    feat: Implementando métodos para IUfRepository

--- src/main/java/br/com/squadra/bootcamp/java/api_spring_boot/uf/IUfRepository.java
+++ src/main/java/br/com/squadra/bootcamp/java/api_spring_boot/uf/IUfRepository.java
@@ -1,6 +1,19 @@
 package br.com.squadra.bootcamp.java.api_spring_boot.uf;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUfRepository extends JpaRepository<UfModel, Long> {
+
+    Boolean existsByCodigoUf(Long codigoUf);
+
+    Boolean existsBySigla(String siglaUf);
+
+    Boolean existsByNome(String nomeUf);
+
+    Optional<UfModel> findByCodigoUf(Long codigoUf);
+
+    Optional<UfModel> findBySigla(String siglaUf);
+    
 }

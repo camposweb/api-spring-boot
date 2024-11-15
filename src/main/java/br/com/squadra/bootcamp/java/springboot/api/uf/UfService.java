8c491c6f2da0ffcfa39495a266e795e56a721c56
Leandro Campos <camposweb@gmail.com>
2024-11-14 21:44:05 -0300

    feat: Finalizando implentações:
    listarUfsPorParametros -> Tem como finalidade listar Ufs por parâmetros que são opcionais.
    
    atualizarUf -> Tem como finalidade atualizar dados da Uf com algumas regras.

--- src/main/java/br/com/squadra/bootcamp/java/springboot/api/uf/UfService.java
+++ src/main/java/br/com/squadra/bootcamp/java/springboot/api/uf/UfService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UfService {
@@ -16,8 +17,28 @@ public class UfService {
     @Autowired
     private List<ValidadorCadastroUf> validadores;
 
-    public List<UfModel> ListarTodasUfs() {
+    public List<UfModel> listarTodasUfs() {
         return this.ufRepository.findAll();
+    }
+
+    public List<UfModel> listarUfsPorParametros(
+        Optional<Long> codigoUf,
+        Optional<String> sigla,
+        Optional<String> nome,
+        Optional<Integer> status
+    ) {
+        
+        if (codigoUf.isPresent()) {
+            return this.ufRepository.findByCodigoUf(codigoUf.get());
+        } else if (sigla.isPresent()) {
+            return this.ufRepository.findBySigla(sigla.get());
+        } else if (nome.isPresent()) {
+            return this.ufRepository.findByNome(nome.get());
+        } else if (status.isPresent()) {
+            return this.ufRepository.findByStatus(status.get());
+        } else {
+            return ufRepository.findAll();
+        }
     }
 
     public List<UfModel> cadastrarUf(UfDTO dadosUf) {
@@ -28,7 +49,10 @@ public class UfService {
 		}
 
         var existeNomeUf = this.ufRepository.existsByNome(dadosUf.nome());
-		if (existeNomeUf) {
+
+        var existeSiglaENome = this.ufRepository.existsBySiglaAndNome(dadosUf.sigla(), dadosUf.nome());
+
+		if (existeSiglaENome) {
 			throw new ValidacaoException("Nome do UF " + dadosUf.nome() + " já existe", 404);
 		}
 
@@ -44,23 +68,21 @@ public class UfService {
 
         var existeCodigoUf = this.ufRepository.existsByCodigoUf(dadosUf.codigoUf());
         if (!existeCodigoUf) {
-            throw new ValidacaoException("UF com o código " + dadosUf.codigoUf() + " não existe", 404);
-        }
-
-        var existeSiglaUf = this.ufRepository.existsBySigla(dadosUf.sigla());
-        if (!existeSiglaUf) {
-            throw new ValidacaoException("Sigla " + dadosUf.sigla() + " não existe", 404);
-        }
-
-        var existeNomeUf = this.ufRepository.existsByNome(dadosUf.nome());
-        if (!existeNomeUf) {
-            throw new ValidacaoException("Nome do UF " + dadosUf.nome() + " já existe", 404);
+            throw new ValidacaoException("codigoUf " + dadosUf.codigoUf() + " informado não existe" , 404);
         }
 
         var atualizarUf = this.ufRepository.getReferenceById(dadosUf.codigoUf());
+
+        var existeSiglaComOutroCodigoUf = this.ufRepository.existsBySiglaAndCodigoUfNot(dadosUf.sigla(), dadosUf.codigoUf());
+        if (existeSiglaComOutroCodigoUf) {
+            throw new ValidacaoException("Sigla " + dadosUf.sigla() + " já existe em outra UF", 404);
+        }
+
         atualizarUf.atualizarInformacoes(dadosUf);
+        this.ufRepository.save(atualizarUf);
 
         return this.ufRepository.findAll();
+        
     }
 
     public List<UfModel> deletarUf(DeletarUfDTO dadosUf) {

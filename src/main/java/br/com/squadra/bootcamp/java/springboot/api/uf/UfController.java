ca18578f2ba7e622e334ff4e5e98b6151e10cca5
Leandro Campos <camposweb@gmail.com>
2024-11-14 21:55:43 -0300

    feat: Implementando rotas:
    listarPorParametrosOpcionais -> Implementa a rota com parâmetros opcionais, sendo que os parâmetros devem ser digitados de forma correto, além das regras que foram solicitadas.
    
    atualizarUf -> Implementa a rota com suas devidas regras

--- src/main/java/br/com/squadra/bootcamp/java/springboot/api/uf/UfController.java
+++ src/main/java/br/com/squadra/bootcamp/java/springboot/api/uf/UfController.java
@@ -1,12 +1,24 @@
 package br.com.squadra.bootcamp.java.springboot.api.uf;
 
-import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import br.com.squadra.bootcamp.java.springboot.api.infra.exception.ValidacaoException;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -16,30 +28,64 @@ public class UfController {
     @Autowired
     private UfService ufService;
 
+    private static final Set<String> PARAMETROS_VALIDOS = Set.of("codigoUf", "sigla", "nome", "status");
+
     @GetMapping
-    public ResponseEntity<List<ListaUfDTO>> listarUf() {
+    public ResponseEntity listarPorParametrosOpcionais(
+        @RequestParam(required = false) Map<String, UfModel> parametros,
+        @RequestParam(required = false) Optional<Long> codigoUf,
+        @RequestParam(required = false) Optional<String> sigla,
+        @RequestParam(required = false) Optional<String> nome,
+        @RequestParam(required = false) Optional<Integer> status
+        ) {
 
-        var ufs = this.ufService.ListarTodasUfs().stream().map(ListaUfDTO::new).toList();
+        List<ListaUfDTO> listarTodasUfs = this.ufService.listarTodasUfs().stream().map(ListaUfDTO::new).toList();
 
-        return ResponseEntity.status(200).body(ufs);
-    }
+            for (String parametro : parametros.keySet()) {
+                if (!PARAMETROS_VALIDOS.contains(parametro)) {
+                    throw new ValidacaoException("Parâmetro " + parametro + " inválido -> Opções codigoUf | sigla | nome | status inscritos exatamente como esá descrito ", 404);
+                }
+
+                if (codigoUf.isPresent()) {
+                    return ResponseEntity.ok().body(this.ufService.listarUfsPorParametros(codigoUf, sigla, nome, status).stream().map(ListaUfDTO::new).findFirst());
+                }
+
+                if (sigla.isPresent()) {
+                    return ResponseEntity.ok().body(this.ufService.listarUfsPorParametros(codigoUf, sigla, nome, status).stream().map(ListaUfDTO::new).findFirst());
+                }
+
+                if (nome.isPresent()) {
+                    return ResponseEntity.ok().body(this.ufService.listarUfsPorParametros(codigoUf, sigla, nome, status).stream().map(ListaUfDTO::new).findFirst());
+                }
+
+                if ((codigoUf.isPresent() || sigla.isPresent() || nome.isPresent()) && status.isPresent()) {
+                    var listaComStatusEOutroParametro = this.ufService.listarUfsPorParametros(codigoUf, sigla, nome, status).stream().map(ListaUfDTO::new).findFirst();
+                    return ResponseEntity.ok().body(listaComStatusEOutroParametro);
+                }
+                
+                 listarTodasUfs = this.ufService.listarUfsPorParametros(codigoUf, sigla, nome, status).stream().map(ListaUfDTO::new).toList();
+                 
+            }
+
+            return ResponseEntity.ok().body(listarTodasUfs);
+        }
 
     @PostMapping
     @Transactional
     public ResponseEntity<List<ListaUfDTO>> cadastrarUf(@RequestBody @Valid UfDTO dadosUf) {
 
-        var ufs = this.ufService.cadastrarUf(dadosUf).stream().map(ListaUfDTO::new).toList();
+        var cadastrarUf = this.ufService.cadastrarUf(dadosUf).stream().map(ListaUfDTO::new).toList();
         
-        return ResponseEntity.status(200).body(ufs);
+        return ResponseEntity.status(200).body(cadastrarUf);
     }
 
     @PutMapping
     @Transactional
     public ResponseEntity<List<ListaUfDTO>> atualizarUf(@RequestBody @Valid AtualizacaoUfDTO dadosUf) {
 
-        var ufs = this.ufService.atualizarUf(dadosUf).stream().map(ListaUfDTO::new).toList();
+        var atualizarUf = this.ufService.atualizarUf(dadosUf).stream().map(ListaUfDTO::new).toList();
 
-        return ResponseEntity.status(200).body(ufs);
+        return ResponseEntity.status(200).body(atualizarUf);
     }
 
     @DeleteMapping
@@ -50,9 +96,9 @@ public class UfController {
             throw new RuntimeException("Código do UF não informado");
         }
 
-        var ufs = this.ufService.deletarUf(dadosUf).stream().map(ListaUfDTO::new).toList();
+        var deletarUf = this.ufService.deletarUf(dadosUf).stream().map(ListaUfDTO::new).toList();
 
-        return ResponseEntity.status(200).body(ufs);
+        return ResponseEntity.status(200).body(deletarUf);
     }
 
 }

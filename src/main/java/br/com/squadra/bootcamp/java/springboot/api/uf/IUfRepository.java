228c49fe940acf1d1eda3f04e2f5dc8fb1097fca
Leandro Campos <camposweb@gmail.com>
2024-11-16 17:54:02 -0300

    feat: Criando algumas consultas na tabela UF

--- src/main/java/br/com/squadra/bootcamp/java/springboot/api/uf/IUfRepository.java
+++ src/main/java/br/com/squadra/bootcamp/java/springboot/api/uf/IUfRepository.java
@@ -1,9 +1,12 @@
 package br.com.squadra.bootcamp.java.springboot.api.uf;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IUfRepository extends JpaRepository<UfModel, Long> {
 
     Boolean existsByCodigoUf(Long codigoUf);
@@ -16,6 +19,8 @@ public interface IUfRepository extends JpaRepository<UfModel, Long> {
 
     Boolean existsByStatus(Integer valor);
 
+    Boolean existsByNomeAndStatus(String nome, Integer status);
+
     List<UfModel> findByCodigoUf(Long codigoUf);
 
     List<UfModel> findBySigla(String siglaUf);
@@ -23,6 +28,8 @@ public interface IUfRepository extends JpaRepository<UfModel, Long> {
     List<UfModel> findByNome(String nomeUf);
 
     List<UfModel> findByStatus(Integer integer);
+
+    Optional<UfModel> findByNomeAndStatus(String nome, Integer status);
 
     Boolean existsBySiglaAndNome(String sigla,String nome);
 

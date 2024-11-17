6c1fc224c0d0e6ad5b456c9b0966348c422a3439
Leandro Campos <camposweb@gmail.com>
2024-11-17 17:26:38 -0300

    feat: Criando AtualizacaoMunicipioDTO

---
+++ src/main/java/br/com/squadra/bootcamp/java/springboot/api/municipio/AtualizacaoMunicipioDTO.java
@@ -0,0 +1,26 @@
+package br.com.squadra.bootcamp.java.springboot.api.municipio;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+public record AtualizacaoMunicipioDTO(
+
+        @NotNull
+        @Range(min = 1, message = "Deve conter o codigoMunicipio referente")
+        Long codigoMunicipio,
+
+        @NotNull
+        @Range(min = 1, message = "Deve conter o codigoUf referente")
+        Long codigoUf,
+
+        @NotBlank
+        @Length(max = 256, message = "Deve conter no máximo 256 caracteres")
+        String nome,
+
+        @NotNull
+        @Range(min = 1, max = 2, message = "Deve conter 1 para ativo ou 2 para desativado")
+        Integer status
+) {
+}

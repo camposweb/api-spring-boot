b80cab32dd3ab9b01cf92ba2eb6b6349b5fa9943
Leandro Campos <camposweb@gmail.com>
2024-11-17 17:54:21 -0300

    fix: Correção DeletarUfDTO

--- src/main/java/br/com/squadra/bootcamp/java/springboot/api/uf/DeletarUfDTO.java
+++ src/main/java/br/com/squadra/bootcamp/java/springboot/api/uf/DeletarUfDTO.java
@@ -1,9 +1,13 @@
 package br.com.squadra.bootcamp.java.springboot.api.uf;
 
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Range;
 
 public record DeletarUfDTO(
+
         @NotNull
+        @Range(min = 1, message = "Deve conter o codigoMunicipio referente")
         Long codigoUf
+
 ) {
 }

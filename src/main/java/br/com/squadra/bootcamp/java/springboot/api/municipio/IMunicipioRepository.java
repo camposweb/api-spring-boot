1e482389247b6402b587d9715f0fd5cb667d09e2
Leandro Campos <camposweb@gmail.com>
2024-11-17 12:04:48 -0300

    feat: Implementando o IMunicipioRepository

--- src/main/java/br/com/squadra/bootcamp/java/springboot/api/municipio/IMunicipioRepository.java
+++ src/main/java/br/com/squadra/bootcamp/java/springboot/api/municipio/IMunicipioRepository.java
@@ -1,9 +1,21 @@
 package br.com.squadra.bootcamp.java.springboot.api.municipio;
 
+import br.com.squadra.bootcamp.java.springboot.api.uf.UfModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IMunicipioRepository extends JpaRepository<MunicipioModel, Long> {
+
+Boolean existsByNome(String nome);
+
+Boolean existsByCodigoUf_CodigoUfAndNome(Long codigoUf, String nome);
+
+Boolean existsByCodigoUf_CodigoUfAndNomeNot(Long codigoUf, String nome);
+
+Optional<MunicipioModel> findByNome(String nome);
+
 
 }

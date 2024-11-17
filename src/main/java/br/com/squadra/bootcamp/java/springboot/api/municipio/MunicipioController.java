15fa8042426175b42b07efaaf7f4961fda953e83
Leandro Campos <camposweb@gmail.com>
2024-11-17 12:06:10 -0300

    feat: Implementando o médoto cadastrarMunicipio e sua rota POST

--- src/main/java/br/com/squadra/bootcamp/java/springboot/api/municipio/MunicipioController.java
+++ src/main/java/br/com/squadra/bootcamp/java/springboot/api/municipio/MunicipioController.java
@@ -2,11 +2,12 @@ package br.com.squadra.bootcamp.java.springboot.api.municipio;
 
 import java.util.List;
 
+import br.com.squadra.bootcamp.java.springboot.api.uf.ListaUfDTO;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/municipio")
@@ -16,12 +17,22 @@ public class MunicipioController {
 	private MunicipioService municipioService;
 
 	@GetMapping
-	public ResponseEntity<List<ListaMunicipioDTO>> getMunicipio() {
+	public ResponseEntity<List<ListaMunicipioDTO>> getmunicipio() {
 
 		var municipios = this.municipioService.listarMunicipios().stream().map(ListaMunicipioDTO::new).toList();
 
 		return ResponseEntity.ok().body(municipios);
 	}
 
+	@PostMapping
+	@Transactional
+	public ResponseEntity cadastrarMunicipio(@RequestBody @Valid MunicipioDTO dadosMunicipio) {
+
+		var cadastrarMunicipio = this.municipioService.cadastrarMunicipios(dadosMunicipio).stream().map(ListaMunicipioDTO::new).toList();
+
+		return ResponseEntity.status(200).body(cadastrarMunicipio);
+
+	}
 
 }
+
--- src/main/java/br/com/squadra/bootcamp/java/springboot/api/municipio/MunicipioService.java
+++ src/main/java/br/com/squadra/bootcamp/java/springboot/api/municipio/MunicipioService.java
@@ -2,17 +2,42 @@ package br.com.squadra.bootcamp.java.springboot.api.municipio;
 
 import java.util.List;
 
+import br.com.squadra.bootcamp.java.springboot.api.infra.exception.ValidacaoException;
+import br.com.squadra.bootcamp.java.springboot.api.uf.IUfRepository;
+import br.com.squadra.bootcamp.java.springboot.api.uf.UfModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MunicipioService {
 
-	@Autowired
-	private IMunicipioRepository municipioRepository;
+    @Autowired
+    private IMunicipioRepository municipioRepository;
 
-	public List<MunicipioModel> listarMunicipios() {
-		return this.municipioRepository.findAll();
-	}
-	
+    @Autowired
+    private IUfRepository ufRepository;
+
+    public List<MunicipioModel> listarMunicipios() {
+        return this.municipioRepository.findAll();
+    }
+
+    public List<MunicipioModel> cadastrarMunicipio(MunicipioDTO dadosMunicipio) {
+
+        var existeCodigoUfTBUF = this.ufRepository.existsByCodigoUf(dadosMunicipio.codigoUf());
+        if (!existeCodigoUfTBUF) {
+            throw new ValidacaoException("codigoUf " + dadosMunicipio.codigoUf() + " informado não existe", 404);
+        }
+
+        var existeMunicipioComCodigoUf = this.municipioRepository.existsByCodigoUf_CodigoUfAndNome(dadosMunicipio.codigoUf(), dadosMunicipio.nome());
+        if (existeMunicipioComCodigoUf) {
+            throw new ValidacaoException("O Município " + dadosMunicipio.nome() + " com codigoUf " + dadosMunicipio.codigoUf() + " já existe", 404);
+        }
+
+
+        var cadastrarMunicipio = new MunicipioModel(dadosMunicipio);
+        this.municipioRepository.save(cadastrarMunicipio);
+
+        return this.municipioRepository.findAll();
+    }
+
 }

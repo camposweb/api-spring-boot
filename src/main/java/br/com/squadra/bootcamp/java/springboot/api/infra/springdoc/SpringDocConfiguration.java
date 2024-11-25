cf8ceab5839a7ec26ef223e6e3e5e22b5b5e4f5a
Leandro Campos <camposweb@gmail.com>
2024-11-25 02:49:30 -0300

    feat: Adicionando Springdoc ao projeto

---
+++ src/main/java/br/com/squadra/bootcamp/java/springboot/api/infra/springdoc/SpringDocConfiguration.java
@@ -0,0 +1,28 @@
+package br.com.squadra.bootcamp.java.springboot.api.infra.springdoc;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Bootcamp Java - Spring Boot")
+                        .description("API para o Bootcamp Java - Spring Boot | Projeto desenvolvido como desafio final do bootcamp java com spring boot da Squadra")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("leandro Campos")
+                                .email("camposweb@gmail.com")
+                                .url("https://github.com/camposweb")
+                        )
+                );
+
+    }
+
+}

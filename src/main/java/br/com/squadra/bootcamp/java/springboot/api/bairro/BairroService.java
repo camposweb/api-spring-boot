b003569edb8d87c82f4127fd559d18dcc27bdce8
Leandro Campos <camposweb@gmail.com>
2024-12-11 19:02:03 -0300

    fix: Otimizando import

--- src/main/java/br/com/squadra/bootcamp/java/springboot/api/bairro/BairroService.java
+++ src/main/java/br/com/squadra/bootcamp/java/springboot/api/bairro/BairroService.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
